6069ed8ed11b7e328a3154f18b26bfcc28ec6c06
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2019-09-11 16:27:54 -0300

    continuando de casa

--- motor/src/scanner.cpp
+++ motor/src/scanner.cpp
@@ -18,6 +18,7 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/io/ply_io.h>
 
 using namespace message_filters;
 using namespace pcl;
@@ -25,84 +26,130 @@ using namespace pcl;
 typedef sync_policies::ApproximateTime<sensor_msgs::LaserScan, nav_msgs::Odometry> syncPolicy;
 
 // Variaveis referentes ao controle do motor
-int raw_min = 133, raw_max = 3979;
+double raw_min = 133, raw_max = 3979;
 double deg_min = 0, deg_max = 360;
-int raw_ref, deg_ref;
+double deg_raw = (deg_max - deg_min) / (raw_max - raw_min);
+double raw_deg = 1.0 / deg_raw;
+double raw_ref, deg_ref;
 ros::Subscriber sub;
 ros::ServiceClient comando_motor;
 bool comecar = false;
 
+// Variaveis para leitura do laser
+laser_geometry::LaserProjection projector;
+
+// Variaveis de nuvem
+PointCloud<PointXYZ>::Ptr acc;
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void chatterCallback(const dynamixel_workbench_msgs::DynamixelStateConstPtr& msg)
 {
-  ROS_INFO("No momento ca estamos: [%d]", msg->present_position);
-  if(abs(msg->present_position - raw_max) < 3){
-      sub.shutdown();
-      ROS_WARN("Chegamos ao fim de curso.");
-  }
+    ROS_INFO("No momento ca estamos: [%d]", msg->present_position);
+    if(abs(msg->present_position - raw_max) < 3){
+        sub.shutdown();
+        ROS_WARN("Chegamos ao fim de curso.");
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+double deg2raw(double deg){
+    return (deg - deg_min)*raw_deg + raw_min;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+double raw2deg(double raw){
+    return (raw - raw_min)*deg_raw + deg_min;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void callback(const sensor_msgs::LaserScanConstPtr& msg_laser,
               const nav_msgs::OdometryConstPtr& msg_motor){
     // Esperar chegar no 0 - ai liberar flag e começar
-    if (abs(msg_motor->pose.pose.position.x) < 2 && !comecar){
-
+    if (abs(msg_motor->pose.pose.position.x - raw_min) < 2 && !comecar){
+        ROS_INFO("Chegamos no inicio para comecar a aquisicao: %.0f", msg_motor->pose.pose.position.x);
+        raw_ref = msg_motor->pose.pose.position.x; // Salva aqui a referencia nova raw
+        //        deg_ref =  // Calcular aqui com a regra de tres necessaria - FUNCAO DEDICADA
+        dynamixel_workbench_msgs::JointCommand comando;
+        comando.request.pan_pos  = raw_max;
+        comando.request.tilt_pos =       0;
+        comando.request.unit     =   "raw";
+        if(comando_motor.call(comando))
+            ROS_INFO("Mandamos para o fim de curso e capturando ao longo do caminho. Estado: %.2f", comando.response.pan_pos);
+        comecar = true;
+    } else if (abs(msg_motor->pose.pose.position.x - raw_min) > 2 && !comecar){
+        ROS_INFO("Estamos esperando chegar no valor minimo, ainda estamos em %.0f", msg_motor->pose.pose.position.x);
     }
 
-    // Começando, converter leitura para nuvem PCL
+    /// DAQUI PRA FRENTE VAI VALER ///
+    if(comecar){
 
-    // Aplicar transformada de acordo com o angulo
+        // Começando, converter leitura para nuvem PCL
+        sensor_msgs::PointCloud2 msg_cloud;
+        projector.projectLaser(*msg_laser, msg_cloud);
+        PointCloud<PointXYZ>::Ptr cloud (new PointCloud<PointXYZ>());
+        fromROSMsg(msg_cloud, *cloud);
+        // Aplicar transformada de acordo com o angulo
 
-    // Acumular nuvem
+        // Acumular nuvem
+        *acc += *cloud;
+
+    } // fim do if comecar, processo principal
+
+    // Finalizar processo se chegar ao fim do curso
+    if(abs(msg_motor->pose.pose.position.x - raw_max) < 2){
+        pcl::io::savePLYFileASCII("/home/grin/Desktop/nuvem_laser.ply", *acc);
+        ROS_WARN("Conferir por nuvem final na area de trabalho.");
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "scanner");
-  ros::NodeHandle nh;
+    ros::init(argc, argv, "scanner");
+    ros::NodeHandle nh;
 
-  sleep(2); // Para os motores iniciarem legal
+    sleep(2); // Para os motores iniciarem legal
 
-  ROS_INFO("Estamos comecando o no agora.");
+    ROS_INFO("Estamos comecando o no agora.");
 
-  /// Inicia o subscriber sincronizado para as mensagens
-  ///
-  message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub(nh, "/scan"                           , 100);
-  message_filters::Subscriber<nav_msgs::Odometry>     motor_sub(nh, "/dynamixel_angulos_sincronizados", 100);
-  Synchronizer<syncPolicy> sync(syncPolicy(100), laser_sub, motor_sub);
-  sync.registerCallback(boost::bind(&callback, _1, _2));
+    /// Aloca o ponteiro da nuvem acumulada
+    ///
+    acc = (PointCloud<PointXYZ>::Ptr) new PointCloud<PointXYZ>();
 
-  /// Inicia o serviço - variavel global, para usar dentro do callback
-  ///
-  comando_motor = nh.serviceClient<dynamixel_workbench_msgs::JointCommand>("/joint_command");
+    /// Inicia o subscriber sincronizado para as mensagens
+    ///
+    message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub(nh, "/scan"                           , 100);
+    message_filters::Subscriber<nav_msgs::Odometry>     motor_sub(nh, "/dynamixel_angulos_sincronizados", 100);
+    Synchronizer<syncPolicy> sync(syncPolicy(100), laser_sub, motor_sub);
+    sync.registerCallback(boost::bind(&callback, _1, _2));
 
-//  // Cliente que ja manda de uma vez de um lado para o outro
-//  ros::ServiceClient rodar_continuo = nh.serviceClient<dynamixel_workbench_msgs::JointCommand>("/joint_command");
+    // Inicio do publicador da nuvem acumulada e mensagem ros
+    ros::Publisher pub_acc = nh.advertise<sensor_msgs::PointCloud2>("/laser_acumulada", 100);
+    sensor_msgs::PointCloud2 msg_acc;
+    msg_acc.header.frame_id = "map";
 
-  /// Mandar motor para o 0 do range
-  ///
-  dynamixel_workbench_msgs::JointCommand comando;
-  comando.request.pan_pos  = raw_min;
-  comando.request.tilt_pos =       0;
-  comando.request.unit     =   "raw";
-  if(comando_motor.call(comando))
-      ROS_INFO("Mandamos a zero para comecar. Estado: %.2f", comando.response.pan_pos);
+    /// Inicia o serviço - variavel global, para usar dentro do callback
+    ///
+    comando_motor = nh.serviceClient<dynamixel_workbench_msgs::JointCommand>("/joint_command");
 
-//  sleep(10); // Aguarda aqui um tempo para que chegue ao 0 com tranquilidade
+    /// Mandar motor para o 0 do range
+    ///
+    dynamixel_workbench_msgs::JointCommand comando;
+    comando.request.pan_pos  = raw_min;
+    comando.request.tilt_pos =       0;
+    comando.request.unit     =   "raw";
+    if(comando_motor.call(comando))
+        ROS_INFO("Mandamos a zero para comecar. Estado: %.2f", comando.response.pan_pos);
 
-  // Subscriber para ir lendo os angulos do motor
-//  sub = nh.subscribe("/multi_port/pan_state", 1000, chatterCallback);
+    ros::Rate r(1);
 
-  // Mandar o motor andar tudo de um lado para o outro mesmo vamos ver como vai ser
-//  comando.request.pan_pos  = raw_max;
-//  comando.request.tilt_pos =       0;
-//  comando.request.unit     =   "raw";
+    while(ros::ok()){
+        // Publicar a nuvem
+        toROSMsg(*acc, msg_acc);
+        msg_acc.header.stamp = ros::Time::now();
+        pub_acc.publish(msg_acc);
+        // Dormir
+        r.sleep();
+        // Rodar o ros - MUITO IMPORTANTE
+        ros::spinOnce();
+    }
 
-//  if(rodar_continuo.call(comando))
-//      ROS_INFO("Mandamos o fim de curso aqui. Estado: %.2f", comando.response.pan_pos);
-
-  ros::spin();
-
-  return 0;
+    return 0;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
