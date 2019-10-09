f881488c62adc250db3179b7fab7c1db943197ea
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2019-10-08 21:45:51 -0300

    projetou imagem ok

--- scanner_gui/src/projetalaser.cpp
+++ scanner_gui/src/projetalaser.cpp
@@ -49,7 +49,7 @@ void ProjetaLaser::set_nuvem(PointCloud<PointT>::Ptr cloud){
 ///////////////////////////////////////////////////////////////////////////////////////////
 void ProjetaLaser::process(){
     // Cria imagem da camera virtual no momento atual de captura
-    cv::Mat foto_virtual(cv::Size(640, 480), CV_64FC1, cv::Scalar(0));
+    cv::Mat foto_virtual(cv::Size(640, 480), CV_8UC3, cv::Scalar(0, 0, 0));
     // Criar matriz de transformacao da nuvem segundo angulo de captura
     Eigen::Matrix3f R;
     R = Eigen::AngleAxisf(                      0, Eigen::Vector3f::UnitX()) *
@@ -59,7 +59,7 @@ void ProjetaLaser::process(){
     T_roty.block<3,3>(0,0) << R.inverse();
     // Transformar nuvem para o frame da camera: matriz anterior e rodar eixos
     transformPointCloud(*nuvem, *nuvem, T_roty*T_eixos);
-    pcl::io::savePLYFileASCII("/home/grin/Desktop/testando.ply", *nuvem);
+    pcl::io::savePLYFileASCII("/home/vinicius/Desktop/testando.ply", *nuvem);
     // Projetar os pontos na foto virtual e colorir imagem
     #pragma omp parallel for num_threads(10)
     for(size_t i = 0; i < nuvem->size(); i++){
@@ -72,16 +72,18 @@ void ProjetaLaser::process(){
         X = X/X(2, 0);
         /// Adicionando ponto na imagem se for o caso de projetado corretamente
         if(floor(X(0,0)) >= 0 && floor(X(0,0)) < foto_virtual.cols && floor(X(1,0)) >= 0 && floor(X(1,0)) < foto_virtual.rows){
-            ROS_INFO("projetou alguem");
             cv::Vec3b cor;
-            cor[0] = nuvem->points[i].r; cor[1] = nuvem->points[i].g; cor[2] = nuvem->points[i].b;
-            foto_virtual.at<cv::Vec3b>(cv::Point(X(1,0), X(0,0))) = cor;
+            cor[0] = nuvem->points[i].b; cor[1] = nuvem->points[i].g; cor[2] = nuvem->points[i].r;
+            foto_virtual.at<cv::Vec3b>(cv::Point(X(0,0), X(1,0))) = cor;
+//            ROS_INFO("projetou alguem, com cores %d   %d   %d", cor[0], cor[1], cor[2]);
         }
     }
     // Salvar foto final para analise
+//    ROS_INFO(" NAO GRAVAMOS IMAGEM");
     char* home;
     home = getenv("HOME");
     std::string nome = std::string(home)+"/Desktop/camera_virtual.jpg";
     cv::imwrite(nome, foto_virtual);
+//    ROS_INFO("GRAVAMOS IMAGEM");
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
